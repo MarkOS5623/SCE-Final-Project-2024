ab5bb5811f7c9d30824c2593f3a3fc13373270fc
MarkOS5623 <markos5623@gmail.com>
2023-12-16 16:02:28 +0200

    Implemented Microsoft Identity into app

---
+++ SCE-Final-Project-2024/Areas/Identity/Data/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+
+// Add profile data for application users by adding properties to the Account class
+public class Account : IdentityUser
+{
+}
+
--- SCE-Final-Project-2024/Program.cs
+++ SCE-Final-Project-2024/Program.cs
@@ -1,32 +1,23 @@
 using Microsoft.EntityFrameworkCore;
-using SCE_Final_Project_2024.Context;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SCE_Final_Project_2024.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AccountDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AccountDbContextConnection' not found.");
 
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddDbContext<AccountDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = options.DefaultPolicy;
-});
+builder.Services.AddDefaultIdentity<Account>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AccountDbContext>();
+
 
 // Add services to the container.
-builder.Services.AddRazorPages()
-    .AddMicrosoftIdentityUI();
+builder.Services.AddRazorPages();
 
 // Builder service that adds account DB context to the database
-builder.Services.AddDbContext<AccountContext>(options =>
-{
-    options.UseSqlServer("Server=DESKTOP-E4BN869;Database=FinalProjectDB;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -42,17 +33,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Remove the following lines
 app.UseAuthentication();
-
 app.UseAuthorization();
 
 // Map Razor Pages and Controllers
 app.MapRazorPages();
 app.MapControllers();
-
-// Map the AccountsController
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
