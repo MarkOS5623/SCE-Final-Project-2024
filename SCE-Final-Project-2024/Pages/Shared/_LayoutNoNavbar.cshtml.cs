d1d61665b7b4004c781977193967f6759814d34f
ITAIDAMRI <2403itai@gmail.com>
2023-12-17 15:12:12 +0200

    redesign logo and navbar

---
+++ SCE-Final-Project-2024/Pages/Shared/_LayoutNoNavbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SCE_Final_Project_2024.Pages.Shared
+{
+    public class _LayoutNoNavbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
