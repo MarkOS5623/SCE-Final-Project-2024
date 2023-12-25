c922bf388d2ac0d47cc026f0ac18f5845a914e0b
MarkOS5623 <markos5623@gmail.com>
2023-12-25 15:59:44 +0200

    Added email sending to registery confirmation and resened confirmetion email.

--- SCE-Final-Project-2024/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ SCE-Final-Project-2024/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -44,36 +44,47 @@ using SCE_Final_Project_2024.Areas.Identity.Data;
         /// </summary>
         public string EmailConfirmationUrl { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(string email, string returnUrl = null)
+    public async Task<IActionResult> OnGetAsync(string email, string returnUrl = null)
+    {
+        if (email == null)
         {
-            if (email == null)
-            {
-                return RedirectToPage("/Index");
-            }
-            returnUrl = returnUrl ?? Url.Content("~/");
-
-            var user = await _userManager.FindByEmailAsync(email);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with email '{email}'.");
-            }
-
-            Email = email;
-            // Once you add a real email sender, you should remove this code that lets you confirm the account
-            DisplayConfirmAccountLink = true;
-            if (DisplayConfirmAccountLink)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                EmailConfirmationUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                    protocol: Request.Scheme);
-            }
-
-            return Page();
+            return RedirectToPage("/Index");
         }
+        returnUrl = returnUrl ?? Url.Content("~/");
+
+        var user = await _userManager.FindByEmailAsync(email);
+        if (user == null)
+        {
+            return NotFound($"Unable to load user with email '{email}'.");
+        }
+
+        Email = email;
+
+        // Check if you want to display the confirmation link
+        DisplayConfirmAccountLink = true;
+
+        if (DisplayConfirmAccountLink)
+        {
+            // Generate email confirmation link
+            var userId = await _userManager.GetUserIdAsync(user);
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+            // Construct the confirmation URL
+            EmailConfirmationUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                protocol: Request.Scheme);
+
+            // Send confirmation email
+            var emailSubject = "Confirm your account";
+            var emailMessage = $"Please confirm your account by clicking <a href='{EmailConfirmationUrl}'>here</a>.";
+
+            await _sender.SendEmailAsync(user.Email, emailSubject, emailMessage);
+        }
+
+        return Page();
     }
+}
 
