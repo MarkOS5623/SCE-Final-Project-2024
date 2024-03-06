d5d66191f2a4e40b68af62fec478f012cd1b6b8c
Mark Grinblat <markos5623@gmail.com>
2024-03-06 19:17:09 +0200

    Added custom toolbar work in progress

--- frontend/src/components/docs/textEditor.js
+++ frontend/src/components/docs/textEditor.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import ReactQuill from 'react-quill';
+import React, {useRef,useEffect } from "react";
+import { Card, Container } from "react-bootstrap";
+import ReactQuill, {handleQuillKeyDown, } from "react-quill";
+import QuillToolbar, { modules, formats } from "./toolbar"; // Import your custom toolbar component
 
 const TextEditor = () => {
+  const quillRef = useRef(null);
+
+  useEffect(() => {
+    const quillInstance = quillRef.current?.getEditor();
+    if (quillInstance) {
+      quillInstance.root.addEventListener("keydown", handleQuillKeyDown);
+    }
+    return () => {
+      if (quillInstance) {
+        quillInstance.root.removeEventListener("keydown", handleQuillKeyDown);
+      }
+    };
+  }, []);
+
   return (
-    <Card style={{ height: '90vh', margin: '2vh', }} bg='primary' text='black'>
-      <Card.Body style={{ height: '100%' }}>
-        <ReactQuill theme="snow" style={{ height: '100%', backgroundColor: 'white', marginBottom: '2vh', overflow: 'auto' }} />
-      </Card.Body>
-    </Card>
+    <Container className="justify-content-center align-items-center">
+      <Card style={{ height: "90vh", margin: "2vh", width: "75%" }} bg="primary" text="black">
+        <Card.Body style={{ height: "100%", overflow: "scroll", }} >
+          <QuillToolbar quill={quillRef.current}  />
+          <ReactQuill
+            ref={quillRef}
+            theme="snow"
+            style={{
+              width: "8.27in", 
+              height: "11.69in", 
+              backgroundColor: "white",
+              margin: "2vh",
+              overflow: 'hidden'
+            }}
+            modules={modules}
+            formats={formats}
+          />
+        </Card.Body>
+      </Card>
+    </Container>
   );
 };
+
 
 export default TextEditor;
