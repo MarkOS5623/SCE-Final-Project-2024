5531d23fa48af3ed0d7ad8b0f667ed55b6883bc8
Mark Grinblat <markos5623@gmail.com>
2024-07-23 16:37:36 +0300

    Reset

--- backend/routes/utilsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const utilsController = require('../controllers/utilsController');
-
-router.post('/decodevalue', utilsController.decodeValue);
-
-module.exports = router;
--- frontend/src/api/config.js
+++
@@ -1,4 +0,0 @@
-export const SERVER_BASE = "http://localhost:5000"
-// localhost:5000/users/auth
-// localhost:5000/documents/
-//  SERVER_BASE    ROUTE_URL
